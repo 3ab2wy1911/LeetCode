df3d34389928dca36486b3d4ad860f6d9ecac7ac
3ab2wy1911 <3ab2wy1911@gmail.com>
2024-01-01 22:58:26 +0200

    2264. Largest 3-Same-Digit Number in String

---
+++ Daily Problems/2023/December/src/Day4/Solution.java
@@ -0,0 +1,13 @@
+package Day4;
+//2264. Largest 3-Same-Digit Number in String
+class Solution {
+    public String largestGoodInteger(String num) {
+        String result = "";
+        for (int i =0;i <num.length()-2;i++){
+            if(num.charAt(i) == num.charAt(i+1) && num.charAt(i+1) == num.charAt(i+2)){
+                if (result.isEmpty() || result.charAt(0) <= num.charAt(i)) result = num.substring(i,i+3);
+            }
+        }
+        return result;
+    }
+}
