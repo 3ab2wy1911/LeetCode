e666640b930382adabf692faa5bb89c69eac6f4d
3ab2wy1911 <3ab2wy1911@gmail.com>
2024-01-01 23:09:23 +0200

    606. Construct String from Binary Tree

---
+++ Daily Problems/2023/December/src/Day9/Solution.java
@@ -0,0 +1,34 @@
+package Day9;
+//94. Binary Tree Inorder Traversal
+import java.util.ArrayList;
+import java.util.List;
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+     TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+ }
+
+class Solution {
+
+    public void inorder (List<Integer>l, TreeNode t){
+        if (t == null) return;
+        inorder(l,t.left);
+        l.add(t.val);
+        inorder(l, t.right);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List <Integer> list = new ArrayList<>();
+        inorder(list,root);
+        return list;
+    }
+}
